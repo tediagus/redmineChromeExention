566889591ffb2a1af00c5184b80b90c7c9b3af00
Tediaga Coulibaly <tediaga.coulibaly@loreal.com>
2022-06-13 14:00:21 +0200

    add redirection on start and finish

--- background.js
+++ background.js
@@ -1,7 +1,1 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() =>{
-    chrome.storage.sync.set({ color});
-
-    console.log('Default background color set to %cgreen', `color: ${color}`)
-})
+// if not use for this momnent
