d043536e6d453b859d36c45a5f48eceb8895f8c9
Bilel JEGHAM <bilel.jegham@niji.fr>
2023-01-24 11:46:02 +0100

    fix: redirection link

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ const selectDates = document.getElementById("selectDates");
 const selectTask = document.getElementById("selectTask");
 const progress = document.getElementById("progress");
 progress.indeterminate = true;
+let projectID
 
 const showElement = (element) => {
   element.classList.remove("hidden");
@@ -22,9 +23,9 @@ const STEP = {
   FINISH: 2
 }
 
-function changeStep(step){
-  
-  switch(step){
+function changeStep(step) {
+
+  switch (step) {
     case STEP.START:
       showElement(form)
       hiddenElement(loading)
@@ -37,20 +38,20 @@ function changeStep(step){
       break;
     case STEP.FINISH:
 
-    getCurrentTab().then((tab)=>{
-      
-      //redirect to time past 
-      const url = gotoRedmine()+'/projects/loreal-dsf-loreal-2022-nf2101863/time_entries?utf8=%E2%9C%93&set_filter=1&sort=spent_on%3Adesc&f%5B%5D=spent_on&op%5Bspent_on%5D=m&f%5B%5D=user_id&op%5Buser_id%5D=%3D&v%5Buser_id%5D%5B%5D=me&f%5B%5D=&c%5B%5D=spent_on&c%5B%5D=user&c%5B%5D=activity&c%5B%5D=issue&c%5B%5D=comments&c%5B%5D=hours&group_by=&t%5B%5D=hours&t%5B%5D='
+      getCurrentTab().then((tab) => {
 
-      if(tab && tab.id){
-        chrome.tabs.update(tab.id, {url: url});
-      } else {
-        chrome.tabs.create({url:url});
-      }
-    })
-   /*    hiddenElement(form)
-      hiddenElement(loading)
-      showElement(finish) */
+        //redirect to time past 
+        const url = gotoRedmine() + '/projects/' + projectID + '/time_entries?utf8=%E2%9C%93&set_filter=1&sort=spent_on%3Adesc&f%5B%5D=spent_on&op%5Bspent_on%5D=m&f%5B%5D=user_id&op%5Buser_id%5D=%3D&v%5Buser_id%5D%5B%5D=me&f%5B%5D=&c%5B%5D=spent_on&c%5B%5D=user&c%5B%5D=activity&c%5B%5D=issue&c%5B%5D=comments&c%5B%5D=hours&group_by=&t%5B%5D=hours&t%5B%5D='
+
+        if (tab && tab.id) {
+          chrome.tabs.update(tab.id, { url: url });
+        } else {
+          chrome.tabs.create({ url: url });
+        }
+      })
+      /*    hiddenElement(form)
+         hiddenElement(loading)
+         showElement(finish) */
       break;
   }
 
@@ -58,15 +59,15 @@ function changeStep(step){
 
 changeStep(STEP.START)
 
-let API_KEY= null;
+let API_KEY = null;
 async function getApiKey() {
-  if(!API_KEY) {
-    try{
-      const text = await fetch("https://redmine.niji.fr/my/api_key").then((a)=> a.text())
+  if (!API_KEY) {
+    try {
+      const text = await fetch("https://redmine.niji.fr/my/api_key").then((a) => a.text())
       //console.log(text)
       API_KEY = text.match(/<pre>(.+)<\/pre>/)[1];
 
-    }catch(e){
+    } catch (e) {
       return null
     }
   }
@@ -76,14 +77,14 @@ async function getApiKey() {
 
 
 
-function generateOptionsForMonth(date = new Date()){
-  const numberOfDays = new Date(date.getFullYear(), date.getMonth()+1 , 0).getDate();
-  for(let i=1; i<=numberOfDays; i++){
-    const dateDay = new Date(date.getFullYear(), date.getMonth() , i+1)
-    
+function generateOptionsForMonth(date = new Date()) {
+  const numberOfDays = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  for (let i = 1; i <= numberOfDays; i++) {
+    const dateDay = new Date(date.getFullYear(), date.getMonth(), i + 1)
+
     const option = document.createElement("option");
-    option.value = dateDay.toISOString().substring(0,10);
-    option.innerText = dateDay.toLocaleString("fr-FR",{dateStyle:"full",timeZone: 'UTC' });
+    option.value = dateDay.toISOString().substring(0, 10);
+    option.innerText = dateDay.toLocaleString("fr-FR", { dateStyle: "full", timeZone: 'UTC' });
     option.disabled = (dateDay.getDay() === 0 || dateDay.getDay() === 1)
 
     selectDates.appendChild(option);
@@ -94,10 +95,10 @@ generateOptionsForMonth();
 
 
 
-async function generateListTask(apiKey, date = new Date()){
+async function generateListTask(apiKey, date = new Date()) {
   const startMonth = new Date(date.getFullYear(), date.getMonth(), 1).toISOString().split('T')[0];
-  const endMonthDate = new Date(date.getFullYear(), date.getMonth()+1 , 0).toISOString().split('T')[0]; 
-  
+  const endMonthDate = new Date(date.getFullYear(), date.getMonth() + 1, 0).toISOString().split('T')[0];
+
   var myHeaders = new Headers();
   myHeaders.append("X-Redmine-API-Key", apiKey);
 
@@ -107,18 +108,18 @@ async function generateListTask(apiKey, date = new Date()){
     redirect: 'follow'
   };
 
-  const issues = (await fetch(`https://redmine.niji.fr/issues.json?start_date=><${startMonth}|${endMonthDate}&limit=100`, requestOptions)
+  const issues = (await fetch(`https://redmine.niji.fr/issues.json?start_date=><${startMonth}|${endMonthDate}&limit=500`, requestOptions)
     .then(response => response.json())).issues
 
-  const tasks = issues.filter((i)=> i.tracker.id === 7).map((t)=>{
-    if(t.parent){
-      t.parentSubject = issues.find((i)=> i.id === t.parent.id).subject 
+  const tasks = issues.filter((i) => i.tracker.id === 7).map((t) => {
+    if (t.parent) {
+      t.parentSubject = issues.find((i) => i.id === t.parent.id).subject
     }
     return t
   })
-   
-  
-  for(let t of tasks){    
+
+
+  for (let t of tasks) {
     const option = document.createElement("option");
     option.value = t.id;
     option.innerText = `${t.fixed_version.name} - ${t.subject} (${t.parentSubject})`;
@@ -128,16 +129,16 @@ async function generateListTask(apiKey, date = new Date()){
 }
 
 
-function gotoRedmine(){
+function gotoRedmine() {
   return 'https://redmine.niji.fr/'
 }
 
 
-async function launch(){
+async function launch() {
 
   const apiKey = await getApiKey()
-  if(!apiKey) {
-    chrome.tabs.create({url: gotoRedmine()});
+  if (!apiKey) {
+    chrome.tabs.create({ url: gotoRedmine() });
   }
   await generateListTask(apiKey)
 }
@@ -153,7 +154,7 @@ form.addEventListener("submit", async (e) => {
   changeStep(STEP.LOADING);
   var myHeaders = new Headers();
   const apiKey = await getApiKey()
- // console.log("submit", apiKey);
+  // console.log("submit", apiKey);
   myHeaders.append("X-Redmine-API-Key", apiKey);
   myHeaders.append("Access-Control-Allow-Origin", "*");
   formData.delete("apiKey");
@@ -170,10 +171,13 @@ form.addEventListener("submit", async (e) => {
     };
 
     await fetch("https://redmine.niji.fr/time_entries.json", requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log(date, result))
-      .catch((error) => console.error(date, error));  
-  }));  
+      .then((response) => response.json())
+      .then((result) => {
+        projectID = result.time_entry.project.name.toLowerCase().replace(/ /g, "-");
+        console.log(date, result)
+      })
+      .catch((error) => console.error(date, error));
+  }));
 
   changeStep(STEP.FINISH);
 
